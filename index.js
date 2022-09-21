8b5fe85b5251be07dd2b93097123399ac8f1466d
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-09-20 21:35:07 -0500

    commit tree c36ead26969e8b0953cf1d8bdb6b810190b78061

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-// Follow along with the examples here
+/*
+function sayHello(){
+    console.log("Hello!");
+}
+sayHello();
+*/
+
+/*
+function sayHelloToGuadalupe(){
+    console.log("Hello, Guadalupe!");
+}
+function sayHelloToLiz(){
+    console.log("Hello, Liz!");
+}
+function sayHelloToSamip(){
+    console.log("Hello, Samip!");
+}
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+*/
+/*
+function sayHelloTo(firstName){
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);  
+
+console.log(firstName);
+*/
+
+/*
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Goodbye", "Julio");
+
+*/
+
+/*
+function add(x,y) {
+    return x + y;
+}
+
+console.log(add(1,2));
+*/
+
+/*
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+}
+
+console.log(say("Hello", "Liz"));
+*/
+/*
+function say(greeting, firstName){
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+console.log(say("Hello", "Liz"));
+*/
+
+/*
+function add(x,y) {
+   return x + y;
+}
+console.log(add(1,3));
+*/
+/*
+const sum = add(num1, num2);
+const message = `The sum of your numbers is: ${add(num1,num2)}`;
+*/
